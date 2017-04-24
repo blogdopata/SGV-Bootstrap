84c2fea88c8e479e2e6f50c7905ffb8ef8ba15b1
blogdopata <vcaballero_c@hotmail.com>
2017-04-24 03:40:26 -0500

    views Piloto + ajax 70%

--- js/callBacks.js
+++ js/callBacks.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
     });
     });
 
-  $('#instructores').on('click', function(){    
-          $.ajax('contents/instructores.html', {                           
-              success: function(response){    
-               $('.contenido').html($(response).find('.mBody').fadeIn());
-              },
-              type: 'POST'     
-      });
-      });
+  // $('#instructores').on('click', function(){    
+  //         $.ajax('contents/instructores.html', {                           
+  //             success: function(response){    
+  //              $('.contenido').html($(response).find('.mBody').fadeIn());
+  //             },
+  //             type: 'POST'     
+  //     });
+  //     });
 
   $(".contenido").on('click','#editarInstructor', function(){    
            $.ajax('contents/editarInstructor.html', {                           
@@ -25,10 +25,47 @@ $(document).ready(function(){
               type: 'POST'  
       });
       });
+  $(".contenido").on('click','#detalleInstructor', function(){    
+           $.ajax('contents/detalleInstructor.html', {                           
+              success: function(response){    
+               $('.contenido').html($(response).find('.mBody').fadeIn());
+              },
+              type: 'POST'  
+      });
+      });
+  $(".container").on('click','#instructores', function(){    
+           $.ajax('contents/Instructores.html', {                           
+              success: function(response){    
+               $('.contenido').html($(response).find('.mBody').fadeIn());
+              },
+              type: 'POST'  
+      });
+      });
 
-  
-
-
+   $(".container").on('click','#listaAlumnos', function(){    
+           $.ajax('contents/listaAlumnos.html', {                           
+              success: function(response){    
+               $('.contenido').html($(response).find('.mBody').fadeIn());
+              },
+              type: 'POST'  
+      });
+      });
+   $(".container").on('click','#detalleAlumno', function(){    
+           $.ajax('contents/detalleAlumno.html', {                           
+              success: function(response){    
+               $('.contenido').html($(response).find('.mBody').fadeIn());
+              },
+              type: 'POST'  
+      });
+      });
+    $(".container").on('click','#editarAlumno', function(){    
+           $.ajax('contents/editarAlumno.html', {                           
+              success: function(response){    
+               $('.contenido').html($(response).find('.mBody').fadeIn());
+              },
+              type: 'POST'  
+      });
+      });
 
 
 });
